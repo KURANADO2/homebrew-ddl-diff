64b3a2bd3942d1157831ea91199eb122e2c048dd
Xinling Jing <jingxinling1024@163.com>
2025-04-18 19:45:14 +0800

    feat: First commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Homebrew Formula for ddl-diff
+
+This is a single-formula Homebrew tap for [`ddl-diff`](https://github.com/KURANADO2/ddl-diff).
+
+## Installation
+
+```bash
+brew install KURANADO2/ddl-diff/ddl-diff
+```
---
+++ ddl-diff.rb
@@ -0,0 +1,13 @@
+class Lazyssh < Formula
+  desc "A tool to compare two databases and generate a diff for MariaDB."
+  homepage "https://github.com/KURANADO2/ddl-diff"
+  url "https://codeload.github.com/KURANADO2/ddl-diff/tar.gz/refs/tags/0.1.5"
+  sha256 "7420acbcd7a6a4dec5c90043f4e1751d253a2d59cb33f8e7a09df23a8bcdfe00"
+  license "MIT"
+
+  depends_on "rust" => :build
+
+  def install
+    system "cargo", "install", *std_cargo_args
+  end
+end
